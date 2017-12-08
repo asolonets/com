b10127d4ae6c2c5c6280c6e8c1f1b80a726b4c30
asolonets <33359723+asolonets@users.noreply.github.com>
2017-12-07 20:10:08 -0800

    New 1-12

---
+++ Promytheus/modules/datadriven/Constants.java
@@ -0,0 +1,7 @@
+package com.Promytheus.modules.datadriven;
+
+public class Constants {
+    public static final String URL = "http://letskodeit.com";
+    public static final String File_Path = "C://Users//Alex//Documents//Selenium//Udemy_//src//datadriven//";
+    public static final String File_Name = "ExcelDataSingleDataSet.xlsx";
+}
